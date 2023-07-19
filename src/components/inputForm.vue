211893f89f12d6c0f1bb68e31331511507cd8725
Madihander <79453431+Madihander@users.noreply.github.com>
2023-07-19 09:53:08 +0600

    Update inputForm.vue
    Add styles

--- src/components/inputForm.vue
+++ src/components/inputForm.vue
@@ -34,3 +34,24 @@ export default {
     }
 };
 </script>
+
+<style scoped>
+*{
+    font-family: Roboto,sans-serif;
+}
+h4{
+    width: 8%;
+    font-size: 24px;
+    font-weight: 600;
+    border-bottom: 4px solid #4922e3;
+}
+input{
+    width: 25%;
+    padding: 10px 5px;
+    font-size: 20px;
+    font-weight: 600;
+    color:#17151f;
+    border: 3px solid #4922e3;
+    border-radius: 4px;
+}
+</style>
